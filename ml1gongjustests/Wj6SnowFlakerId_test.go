54df97e4221b9bdd2dab3b271ace8a153acfc891
changlizhi <changlizhi@aliyun.com>
2020-08-16 14:46:51 +0800

    多并发获取id

--- ml1gongjustests/Wj6SnowFlakerId_test.go
+++ ml1gongjustests/Wj6SnowFlakerId_test.go
@@ -3,14 +3,19 @@ import(
   "log"
   "testing"
   "xm0jichu/ml0gongjus"
+  "time"
 )
 func TestHuoquId(t *testing.T){
   ret := ml0gongjus.HuoquId()
   log.Println(ret)
 }
 func TestHuoQuIdZiFu(t *testing.T){
-  for i:=0;i<40;i++{
-    ret := ml0gongjus.HuoQuIdZiFu()
-    log.Println(ret,len(ret))
+  for i:=0;i<4000;i++{
+    go func(i int){
+      ret := ml0gongjus.HuoQuIdZiFu()
+      log.Println("TestHuoQuIdZiFu---",i,ret,len(ret))
+    }(i)
   }
+  time.Sleep(time.Duration(5) * time.Hour)
+  
 }
