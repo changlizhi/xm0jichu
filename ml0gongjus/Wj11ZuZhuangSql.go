c9f85a13aa974918eae2d9e5bc485698cd4e0567
changlizhi <changlizhi@aliyun.com>
2020-08-17 01:54:34 +0800

    迁入工具

---
+++ ml0gongjus/Wj11ZuZhuangSql.go
@@ -0,0 +1,23 @@
+package ml0gongjus
+
+import(
+"xm0jichu/ml2changliangs"
+)
+
+func ZuZhuang20BIGINT(mingCheng string)map[string]interface{}{
+  ret := map[string]interface{}{}
+	ret[ml2changliangs.BiaoMing] = mingCheng
+	ret[ml2changliangs.LeiXing] = ml2changliangs.BIGINT
+	ret[ml2changliangs.ChangDu] = "20"
+	ret[ml2changliangs.MoRenZhi] = ml2changliangs.Zf0
+  return ret
+}
+func ZuZhuang50VARCHAR(mingCheng string)map[string]interface{}{
+  ret := map[string]interface{}{}
+  ret[ml2changliangs.BiaoMing] = mingCheng
+  ret[ml2changliangs.LeiXing] = ml2changliangs.VARCHAR
+  ret[ml2changliangs.ChangDu] = "50"
+  ret[ml2changliangs.MoRenZhi] = ml2changliangs.FhDanYinHao + ml2changliangs.HFXXIAOXIE + ml2changliangs.FhDanYinHao
+  return ret
+}
+
--- ml6kustests/Wj2BiaoCaoZuo_test.go
+++ ml6kustests/Wj2BiaoCaoZuo_test.go
@@ -6,6 +6,7 @@ import (
 	"xm0jichu/ml2changliangs"
 	"xm0jichu/ml3moxings"
 	"xm0jichu/ml5kus"
+	"xm0jichu/ml0gongjus."
 )
 
 // 字段表：主键，名称，编码，是否指定，字段值表，是否有行为，校验
@@ -15,22 +16,6 @@ import (
 // 方法表：主键，方法名，必须参数，可选参数，返回数据，描述。
 // 这几个基础表将会是整个系统的产生地。
 
-func zuZhuang20BIGINT(mingCheng string)map[string]interface{}{
-  ret := map[string]interface{}{}
-	ret[ml2changliangs.BiaoMing] = mingCheng
-	ret[ml2changliangs.LeiXing] = ml2changliangs.BIGINT
-	ret[ml2changliangs.ChangDu] = "20"
-	ret[ml2changliangs.MoRenZhi] = ml2changliangs.Zf0
-  return ret
-}
-func zuZhuang50VARCHAR(mingCheng string)map[string]interface{}{
-  ret := map[string]interface{}{}
-  ret[ml2changliangs.BiaoMing] = mingCheng
-  ret[ml2changliangs.LeiXing] = ml2changliangs.VARCHAR
-  ret[ml2changliangs.ChangDu] = "50"
-  ret[ml2changliangs.MoRenZhi] = ml2changliangs.FhDanYinHao + ml2changliangs.HFXXIAOXIE + ml2changliangs.FhDanYinHao
-  return ret
-}
 
 func TestChuangJianBiaoMingBiao(t *testing.T) {
 	// 表名表：主键，名称，编码，主键表
@@ -50,16 +35,16 @@ func TestChuangJianBiaoMingBiao(t *testing.T) {
 
 	ziDuans := []map[string]interface{}{}
 
-	ziDuan1 := zuZhuang20BIGINT(ml2changliangs.ZhuJian)
+	ziDuan1 := ml0gongjus.ZuZhuang20BIGINT(ml2changliangs.ZhuJian)
 	ziDuans = append(ziDuans, ziDuan1)
 
-	ziDuan2 := zuZhuang50VARCHAR(ml2changliangs.MingCheng)
+	ziDuan2 := ml0gongjus.ZuZhuang50VARCHAR(ml2changliangs.MingCheng)
 	ziDuans = append(ziDuans, ziDuan2)
 
-	ziDuan3 := zuZhuang50VARCHAR(ml2changliangs.BianMa)
+	ziDuan3 := ml0gongjus.ZuZhuang50VARCHAR(ml2changliangs.BianMa)
 	ziDuans = append(ziDuans, ziDuan3)
 
-	ziDuan4 := zuZhuang50VARCHAR(ml2changliangs.ZhuJianBiao)
+	ziDuan4 := ml0gongjus.ZuZhuang50VARCHAR(ml2changliangs.ZhuJianBiao)
 	ziDuans = append(ziDuans, ziDuan4)
 
 	ziDuansKeyMap := map[string]interface{}{
