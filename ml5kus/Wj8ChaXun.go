26ce3b6d26ab9ef799309d088eb4c0714f0cd4fe
changlizhi <changlizhi@aliyun.com>
2020-08-25 09:48:18 +0800

    根据排序后的字段传入之后返回的字段名应该是相同的，后续做个判断，如此就把数据存入map格式了

--- ml5kus/Wj8ChaXun.go
+++ ml5kus/Wj8ChaXun.go
@@ -35,8 +35,10 @@ func ChaXun(canShu ml3moxings.CanShu)ml3moxings.CanShu{
   builder.WriteString(caoZuoKu)
   builder.WriteString(ml2changliangs.FhDianHao)
   builder.WriteString(caoZuoBiao)
-  builder.WriteString(" WHERE ")
-  builder.WriteString(strings.Join(wenHaos," AND "))
+  if len(wenHaos) > ml2changliangs.Sz0{
+    builder.WriteString(" WHERE ")
+    builder.WriteString(strings.Join(wenHaos," AND "))
+  }
   
   sqlStr := builder.String()
   dbCanShuRet := HuoQuLianJieChi(caoZuoKu)
@@ -44,6 +46,34 @@ func ChaXun(canShu ml3moxings.CanShu)ml3moxings.CanShu{
   
   rows, err := db.Query(sqlStr,values...)
   
-  log.Println("XinZeng:sqlStr,values,rows,err---", sqlStr,values,rows, err)
+  //在没有报错的情况下可以执行Next和Scan方法把所有查到的字段值拿出来放到CanShu里返回回去
+  retShuJu := scanRet(chaXunZiDuans,rows)
+  ret := ml3moxings.CanShu{
+    ShuJu:retShuJu,
+  }
+  log.Println("XinZeng:sqlStr,values,err,ret---", sqlStr,values, err,ret)
   return canShu
+}
+
+func scanRet(lies []string,rows *sql.Rows)[]map[string]interface{}{
+	ret := []map[string]interface{}{}
+	for rows.Next() {//每一行
+		tempLie := make([]string,len(lies))
+    tempLieRef := make([]interface{},len(lies))
+
+    for i := ml2changliangs.Sz0;i < len(lies); i++{
+      tempLieRef[i] = &tempLie[i]
+    }
+    rows.Scan(
+      tempLieRef...,
+    )
+		retOne := map[string]interface{}{}
+		for xiaBiao,lie := range lies{
+      if tempLieRef[xiaBiao] !=  nil{
+        retOne[lie]=*tempLieRef[xiaBiao].(*string)
+      }
+		}
+		ret = append(ret,retOne)
+	}
+	return ret
 }
